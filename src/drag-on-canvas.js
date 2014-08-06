5402eb569b23128278a13c574c308ac28a3b68dd
andrewcrab <Andrew@AndrewXiedeMacBook-Air.local>
2014-08-06 20:51:40 +0800

    Improve canvas

---
+++ src/drag-on-canvas.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Andrew on 14-8-6.
+ */
+
+//Some Objects that can be dragged
+var Rect = function(x,y,width,height,color){
+    ac.canvas
+        .setFillStyle(color)
+        .fillRect(x,y,width,height);
+};
+
