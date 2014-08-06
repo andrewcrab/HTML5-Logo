75f8f3ac406e23f707da09d2c0279fd12009e399
andrewcrab <Andrew@AndrewXiedeMacBook-Air.local>
2014-08-06 17:40:08 +0800

    Add poly and style to ac.canvas

--- src/main.js
+++ src/main.js
@@ -5,27 +5,27 @@ var ctx;
 var factor = 1;
 var fontFamily = "65px 'Gill Sans Ultra Bold', sans-serif";
 
-init();
+//init();
+//
+//function init(){
+//    ctx= this.document.getElementById("canvas").getContext("2d");
+//    ctx.font = fontFamily;
+//    ctx.save();
+//    dologo();
+//};
+//
+//function dologo(){
+//    var offsety = 80;
+//    ctx.restore();
+//    ctx.save();
+//
+//    ctx.clearRect(0,0,600,400);
+//    ctx.scale(factor,factor);
+//
+//    ctx.fillText("HTML5", 31,60);
+//
+//
+//};
 
-function init(){
-    ctx= this.document.getElementById("canvas").getContext("2d");
-    ctx.font = fontFamily;
-    ctx.save();
-    dologo();
-};
-
-function dologo(){
-    var offsety = 80;
-    ctx.restore();
-    ctx.save();
-
-    ctx.clearRect(0,0,600,400);
-    ctx.scale(factor,factor);
-
-    ctx.fillText("HTML5", 31,60);
-
-
-};
-
-ac.canvas.drawText("HTML5", 31,60);
-this.ac.canvas
+//ac.canvas.drawText("HTML5", 31,60);
+//this.ac.canvas
