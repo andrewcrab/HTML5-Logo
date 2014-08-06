751d2b40a71612f62c65ac87dec3a185c0e1e5df
andrewcrab <Andrew@AndrewXiedeMacBook-Air.local>
2014-08-06 16:18:23 +0800

    Draw HTML5, bad structure

--- src/main.js
+++ src/main.js
@@ -1,3 +1,27 @@
 /**
  * Created by Andrew on 14-8-6.
  */
+var ctx;
+var factor = 1;
+var fontFamily = "65px 'Gill Sans Ultra Bold', sans-serif";
+
+init();
+
+function init(){
+    ctx= this.document.getElementById("canvas").getContext("2d");
+    ctx.font = fontFamily;
+    ctx.save();
+    dologo();
+};
+
+function dologo(){
+    var offsety = 80;
+    ctx.restore();
+    ctx.save();
+
+    ctx.clearRect(0,0,600,400);
+    ctx.scale(factor,factor);
+
+    ctx.fillText("HTML5", 31,60);
+};
+
