02bddcf0d9457a255d6dd960e13cf412752cfdae
andrewcrab <Andrew@AndrewXiedeMacBook-Air.local>
2014-08-07 13:37:35 +0800

    Circle center jumps when circle is selected, need tuning.

--- src/drag-on-canvas.js
+++ src/drag-on-canvas.js
@@ -2,4 +2,5 @@
  * Created by Andrew on 14-8-6.
  */
 
-ac.draggable.Ball.create(100,100,30).draw();
+ac.draggable.Ball.create(100,100,30,'blue').draw();
+ac.draggable.Ball.create(200,200,40,'green').draw();
