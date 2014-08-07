7928e465defe631cad1749a2b401bc46d1aa7512
andrewcrab <Andrew@AndrewXiedeMacBook-Air.local>
2014-08-07 08:40:49 +0800

    Add Image class, not working

--- src/drag-on-canvas.js
+++ src/drag-on-canvas.js
@@ -3,5 +3,33 @@
  */
 
 //Some Objects that can be dragged
-ac.canvas.drawImage("resource/pic1.jpg",0,0);
+//var  i = ac.draggable.Image("resource/pic1.jpg",0,0);
+//i.draw();
+//img = ac.canvas.Image("resource/pic1.jpg",0,0).draw();
+//var b = new ac.canvas.Ball(100,100,20,'red');
+//b.draw();
 
+//var C = function(){
+//    this.write = function(){
+//        console.log("Hello");
+//    }
+//}
+
+//A = ac.dr
+//
+//var c = new C();
+//c.write();
+//ac.canvas.
+//function Image(src,x,y){
+//    this.src = src;
+//    this.x = x;
+//    this.y = y;
+//
+//    this.draw = function(){
+//        ac.canvas.drawImage(this.src,this.x,this.y);
+//    };
+//};
+//
+////var i = new Image("resource/pic1.jpg",0,0);
+////i.draw();
+//ac.canvas.drawImage("resource/pic1.jpg",0,0);
