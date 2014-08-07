b3f94be94c7eff2ccdb7f87f1b7532c9f2f89dca
andrewcrab <Andrew@AndrewXiedeMacBook-Air.local>
2014-08-07 09:34:18 +0800

    Add draggle ball

--- src/drag-on-canvas.js
+++ src/drag-on-canvas.js
@@ -2,34 +2,5 @@
  * Created by Andrew on 14-8-6.
  */
 
-//Some Objects that can be dragged
-//var  i = ac.draggable.Image("resource/pic1.jpg",0,0);
-//i.draw();
-//img = ac.canvas.Image("resource/pic1.jpg",0,0).draw();
-//var b = new ac.canvas.Ball(100,100,20,'red');
-//b.draw();
 
-//var C = function(){
-//    this.write = function(){
-//        console.log("Hello");
-//    }
-//}
-
-//A = ac.dr
-//
-//var c = new C();
-//c.write();
-//ac.canvas.
-//function Image(src,x,y){
-//    this.src = src;
-//    this.x = x;
-//    this.y = y;
-//
-//    this.draw = function(){
-//        ac.canvas.drawImage(this.src,this.x,this.y);
-//    };
-//};
-//
-////var i = new Image("resource/pic1.jpg",0,0);
-////i.draw();
-//ac.canvas.drawImage("resource/pic1.jpg",0,0);
+ac.draggable.Ball.create(100,100,20).draw();
